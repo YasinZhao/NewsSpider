09ff46aa7ee2db99ca57b32d3c4c814544e285a7
yasin <yasin.zhao@outlook.com>
2023-10-15 11:54:33 +0800

    update config

--- twitter_api_service.py
+++ twitter_api_service.py
@@ -1,18 +1,21 @@
 import os
 import json
+import yaml
 import requests
 from requests_oauthlib import OAuth1Session
 
 
 class TwitterClient:
     def __init__(self):
-        self.consumer_key = "Mq8IiiRrUnGsxV0wGjbF0upe5"
-        self.consumer_secret = "sUQmuCzABCoV2CYTW4zmUCJcL76O9m8YYNmc2kFFbrnmbOEDLX"
-        self.bearer_token = "AAAAAAAAAAAAAAAAAAAAAH36qQEAAAAA1D0z0rruIRVULQPK5W0Y9rG148s%3D0paTwokB5c8HlGqF41m9zRkgV0G19AV7OmW4VULAiiHLpyM5O3"
-        self.access_token = "1712790134725427200-I6lK0eNlLOdA6LYEQUW8VTqmhiT0rr"
-        self.access_token_secret = "MwdMhqtfqizQgFEup2XytCyFLP6oXeomDQVAbddnUqdQD"
-        self.client_id = "MWMyYzBYc3JGcHZCOVd1aHFvTE06MTpjaQ"
-        self.client_secret = "aKHvdu1DfuxPfKvj0WlnY4XYPDTuGmGHlNWkxLYGKH4HrUYhJT"
+        with open("config/twitter_api.yaml", "r") as f:
+            config = yaml.safe_load(f)
+        self.consumer_key = config['twitter-api']['consumer_key']
+        self.consumer_secret = config['twitter-api']['consumer_secret']
+        self.bearer_token = config['twitter-api']['bearer_token']
+        self.access_token = config['twitter-api']['access_token']
+        self.access_token_secret = config['twitter-api']['access_token_secret']
+        self.client_id = config['twitter-api']['client_id']
+        self.client_secret = config['twitter-api']['client_secret']
 
     def create_client(self):
         oauth_client = OAuth1Session(
@@ -33,7 +36,7 @@ class TwitterClient:
 
 
 if __name__ == '__main__':
-    TwitterClient().post_tweets()
+    TwitterClient()
 
 
 
